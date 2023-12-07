1cd20970bdf60725380a0ee86d4bc589b60fa486
Sahin2002 <shahinderishov@iclound.com>
2023-12-08 00:48:39 +0300

    finis

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let nav=document.querySelector('nav')
+let play=document.querySelector('.videos-player')
+AOS.init();
 function opens(){
     console.log(nav)
     nav.classList.toggle('nav')
+}
+function players() {
+    play.style='display:flex;'
+}
+function closes() {
+    play.style='display:none;'
 }
